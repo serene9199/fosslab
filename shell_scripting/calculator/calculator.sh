2bf61f395d8cf9d69689e7fb07b04a13ec909302
serene9199 <serene98mec@gmail.com>
2018-04-02 20:53:36 +0530

    Updated with shell script for calculator

---
+++ shell_scripting/calculator/calculator.sh
@@ -0,0 +1,42 @@
+clear
+ans=0
+o="y"
+
+echo "Enter the first no:"
+read n1
+echo "Enter the second no:"
+read n2
+
+while [ $o != "n" ]
+do
+echo "Enter your choice"
+echo "1. Addition"
+echo "2. Substraction"
+echo "3. Multiplication"
+echo "4. Division"
+read ch
+if [ $ch = "1" ]
+then
+ ans=$((n1 + n2))
+ echo "result "$ans
+elif [ $ch = "2" ]
+then
+ ans=$((n1 - n2))
+ echo "result "$ans
+elif [ $ch = "3" ]
+then
+ ans=$((n1 * n2))
+ echo "result "$ans
+else :
+ ans=$((n1 / n2))
+ echo "result "$ans
+  
+fi
+echo "Do you want to continue?(y/n)"
+read o
+if [ $o == "n" ]
+then 
+ exit
+fi
+ done
+
